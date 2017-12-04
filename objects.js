426f90bc86fefe27750b167334a28a0c21c970a7
Christina Collura <cmcollura@gmail.com>
2017-12-04 21:54:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,5 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
-  return delete playlist.artistName;
+  var newPlaylist = delete playlist.artistName;
+  return newPlaylist;
 }
