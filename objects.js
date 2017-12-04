c64cc7558c0ce4f49b3cea737672e1be31d520a7
Christina Collura <cmcollura@gmail.com>
2017-12-04 22:03:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-Object.assign(playlist, {[artistName]: songTitle});
+Object.assign({playlist}, {[artistName]: songTitle});
 
 function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle;
